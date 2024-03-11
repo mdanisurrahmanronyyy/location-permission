ba61b2062ce31a1fdd3fe49c72171027e86c2562
Md. Anisur Rahman Rony <anisur.rony@technonext.com>
2024-03-11 23:10:05 +0600

    init script

--- src/app/LocationComponent.js
+++ src/app/LocationComponent.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Script from 'next/script'
 
 const LocationComponent = () => {
   return (
     <div>
       <h1>Location Component</h1>
+      <Script src="https://cdn.us-bangla.com/user-tracking/tn-maps.js" />
     </div>
   )
 }
